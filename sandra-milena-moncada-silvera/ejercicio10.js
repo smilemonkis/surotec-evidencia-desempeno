0ea57d6caef79613ec7927db1be94a0b6da2734f
Milena Moncada <smilemonkis@gmail.com>
2026-01-30 09:49:32 -0500

    Ejercicio 10:Corrección  Conversor con Registro

---
+++ sandra-milena-moncada-silvera/ejercicio10.js
@@ -0,0 +1,29 @@
+let contadorConversiones = 0
+
+export function convertirMoneda(monto, origen, destino) {
+
+    let resultado = 0
+
+    let montoEnCop = 0
+    if (origen === "COP") {
+        montoEnCop = monto
+    } else if (origen === "USD") {
+        montoEnCop = monto * 4000
+    } else if (origen === "EUR") {
+        montoEnCop = monto * 4300
+    }
+
+    if (destino === "COP") {
+        resultado = montoEnCop
+    } else if (destino === "USD") {
+        resultado = montoEnCop / 4000
+    } else if (destino === "EUR") {
+        resultado = montoEnCop / 4300
+    }
+
+    contadorConversiones = contadorConversiones + 1
+    
+    console.log("Conversiones realizadas: " + contadorConversiones)
+    
+    return resultado.toFixed(2)
+}
